d9886e459db7a87d8e3bb7de73db55a2e788c8b4
marshall brock <marshallbrock@gmail.com>
2020-06-14 17:48:56 -0500

    updating

---
+++ Brick Breaker/Assets/Scripts/GameStatus.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameStatus : MonoBehaviour
+{
+    //this will have a slider serializer
+    [Range(0.1f, 10f)][SerializeField] float gameSpeed = 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //this is saying the slider you made above referenes the time scale
+        Time.timeScale = gameSpeed;
+    }
+}
+
+
