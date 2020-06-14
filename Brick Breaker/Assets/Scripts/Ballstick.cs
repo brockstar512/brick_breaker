e2c18af4f15630677596310810646cc7d88aff09
marshall brock <marshallbrock@gmail.com>
2020-06-13 23:01:07 -0500

    saving

--- Brick Breaker/Assets/Scripts/Ballstick.cs
+++ Brick Breaker/Assets/Scripts/Ballstick.cs
@@ -5,9 +5,14 @@ using UnityEngine;
 public class Ballstick : MonoBehaviour
 {
     //config peremters
+            //this is data type Paddle cause its referencing a script
     [SerializeField] Paddle paddle1;
+    [SerializeField] float xPush = 2f;
+    [SerializeField] float yPush = 15f;
     //state
     Vector2 paddleToBallVector;
+    bool hasStarted = false;
+
 
     // Start is called before the first frame update
     void Start()
@@ -19,7 +24,16 @@ public class Ballstick : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //glue things together... this is a variable that equals wherever the paddle equals on the x and y
+        //this without an edgecase is making the ball locked every update so we need to create a boolian
+      if(!hasStarted)
+      {
+        LockBallToPaddle();
+        BallLaunchOnClick();
+      }
+    }
+    private void LockBallToPaddle()
+    {
+          //glue things together... this is a variable that equals wherever the paddle equals on the x and y
         Vector2 paddlePos = new Vector2(paddle1.transform.position.x,paddle1.transform.position.y);
         //now for the ball
         //ball.position             =     the paddle coordinants + the difference of start paadle and start ball   
@@ -27,5 +41,21 @@ public class Ballstick : MonoBehaviour
         transform.position = paddlePos + paddleToBallVector;
         //right now there is a little lag between the ball moving with the paddle. We can fix that by
         //going to edit -> project settings -> script execution order-> 
+
+    }
+    private void BallLaunchOnClick()
+    {           //if the left mouse button is clicked
+        if(Input.GetMouseButtonDown(0))
+        {
+            hasStarted = true;
+            //in order to launch ball we need access to its rigid body component
+            //rigid body is what has the ball respond to gravity and physics 
+            //this kind of component is a little more tricky to get ahold of.
+            GetComponent<Rigidbody2D>().velocity = new Vector2 (xPush, yPush);
+                                //this is giving the ball a velocity in the x and y
+
+            
+        }
+        
     }
 }
---
+++ Brick Breaker/Assets/Scripts/Block.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour
+{
+    //when something collides to whatever holds this script...
+    //were passing in a variable type of collison
+    //there is trigger and collision
+   private void OnCollisionEnter2D(Collision2D collision) {
+       //the game object that holds this script... i could create a 1 second delay by typing Destroy(gameObject, 1f) 
+       //the collision variable just holds information about what it collides with and info about the collision... I think
+       Destroy(gameObject);
+   }
+}
